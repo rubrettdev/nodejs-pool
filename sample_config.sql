f5f18bdd4956ed583a57b5d3e0386b959b949b90
RBrettes <rui.brettes+dev@gmail.com>
2018-01-15 10:44:06 +0000

    no message

--- sample_config.sql
+++ sample_config.sql
@@ -1,6 +1,6 @@
-UPDATE pool.config SET item_value = '' WHERE module = 'pool' and item = 'address';
-UPDATE pool.config SET item_value = '' WHERE module = 'payout' and item = 'feeAddress';
+UPDATE pool.config SET item_value = 'iz52Qvsm2t1SfMJye2LnDKjJZiE9qyPcoCdXJKKvwCVwNsPRPDbR6XFP2H2SdpNaxsar39Cq3ettBDX9vRrot8eA11Yx2Qahj' WHERE module = 'pool' and item = 'address';
+UPDATE pool.config SET item_value = 'iz52Qvsm2t1SfMJye2LnDKjJZiE9qyPcoCdXJKKvwCVwNsPRPDbR6XFP2H2SdpNaxsar39Cq3ettBDX9vRrot8eA11Yx2Qahj' WHERE module = 'payout' and item = 'feeAddress';
 UPDATE pool.config SET item_value = '' WHERE module = 'general' and item = 'mailgunKey';
 UPDATE pool.config SET item_value = '' WHERE module = 'general' and item = 'mailgunURL';
 UPDATE pool.config SET item_value = '' WHERE module = 'general' and item = 'emailFrom';
-UPDATE pool.config SET item_value = '' WHERE module = 'general' and item = 'shareHost';
+UPDATE pool.config SET item_value = 'http://10.0.0.131/leafApi' WHERE module = 'general' and item = 'shareHost';
